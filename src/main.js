588e460dcf28880c194491667f07285ecd90f4af
LaurenAnds <laurenjayne.ands@outlook.com>
2024-01-24 10:58:01 +0000

    first commit

--- src/main.js
+++ src/main.js
@@ -1,5 +1,56 @@
-import { sum } from "./sum.js";
+var QRCode = require('qrcode')
 
-console.log("sum is ", sum(10, 20));
-console.log("This is main.js.");
-console.log("You should probably use `yarn test` or `yarn test --watchAll`");
+let channelNames = ["codeorg",
+"CoderbyteDevelopers",
+"Codesmith",
+"craigndave",
+"decomplexify",
+"Fireship",
+"freecodecamp",
+"funfunfunction",
+"KevinPowell",
+"NetNinja",
+"TraversyMedia",
+"WesBos"]
+
+function codeToFile(ytLink, chName){
+QRCode.toFile(`src/outputImages/QRcode_` + chName + '.png', ytLink, {
+    color: {
+      dark: '#FFF',  // Blue dots
+      light: '#0000' // Transparent background
+    }
+  }, function (err) {
+    if (err) throw err
+    console.log('done')
+  })
+  console.log(ytLink)
+};
+
+  //links 
+  const youtubeLinks = [
+    "https://www.youtube.com/@codeorg",
+    "https://www.youtube.com/@CoderbyteDevelopers",
+    "https://www.youtube.com/@Codesmith",
+    "https://www.youtube.com/@craigndave",
+    "https://www.youtube.com/@decomplexify",
+    "https://www.youtube.com/@Fireship",
+    "https://www.youtube.com/@freecodecamp",
+    "https://www.youtube.com/@funfunfunction",
+    "https://www.youtube.com/@KevinPowell",
+    "https://www.youtube.com/@NetNinja",
+    "https://www.youtube.com/@TraversyMedia",
+    "https://www.youtube.com/@WesBos"
+]
+
+async function createCode(inputArray){
+    let i = 0;
+    console.log("Start process");
+    for(let element of inputArray){
+        let chName = channelNames[i];
+        await codeToFile(element, chName);
+        chName++;
+        
+    }
+}
+
+createCode(youtubeLinks);
